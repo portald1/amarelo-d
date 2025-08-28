4ca5308872041be7eea515c5c9fdd7b450f3465f
portald1 <anonidolphin@gmail.com>
2025-08-27 21:07:20 -0300

    Add files via upload

---
+++ 2/js/pixel.js
@@ -0,0 +1,21 @@
+(function () {
+window.googlePixelId = "68af1a51390b0da1784d4f3e";
+  var a = document.createElement("script");
+  a.setAttribute("async", "");
+  a.setAttribute("defer", "");
+  a.setAttribute("src", "https://cdn.utmify.com.br/scripts/pixel/pixel-google.js");
+  document.head.appendChild(a);
+
+})();
+
+(function () {
+  const script = document.createElement("script");
+
+  script.src = "https://cdn.utmify.com.br/scripts/utms/latest.js";
+  script.setAttribute("data-utmify-prevent-xcod-sck", "");
+  script.setAttribute("data-utmify-prevent-subids", "");
+  script.async = true;
+  script.defer = true;
+
+  document.head.appendChild(script);
+})();
